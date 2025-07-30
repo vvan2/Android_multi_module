44fca431139846e8ce786c0505b0b1978c939623
Son Juwan <wndhks171@naver.com>
2025-07-30 18:59:18 +0900

    refact to compose

---
+++ app/src/main/java/org/umc/eatthis_compose/core/designsystem/component/MainTopBar.kt
@@ -0,0 +1,16 @@
+package org.umc.eatthis_compose.core.designsystem.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+private fun MainTopBarPreview() {
+    MainTopBar()
+}
+
+@Composable
+fun MainTopBar(
+
+) {
+}
